63f212de5226bf89396ba7d92cc7b51720266c9e
candreanivera <candreanivera@gmail.com>
2024-08-09 16:16:56 -0700

    adding first Endpoint + gitignore file

--- ExpiredFood/Program.cs
+++ ExpiredFood/Program.cs
@@ -1,6 +1,17 @@
+using ExpiredFood.DTO;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<CategoryDTO> categories = [
+new(1, "Fruits"),
+new(2, "Bread"),
+new(3, "Vegetables")
+];
+
 app.MapGet("/", () => "Hello World!");
+
+//Endpoint tto list all the Categories
+app.MapGet("/categories", () => categories);
 
 app.Run();
